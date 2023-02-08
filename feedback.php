e3c02eee7aaa9306595cd4d15eb51b807d805ba6
JairoCalderon <j.calderoneng@gmail.com>
2023-02-08 15:28:26 -0500

    adding header and footer modules

--- feedback.php
+++ feedback.php
@@ -11,28 +11,9 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-sm navbar-light bg-light mb-4">
-        <div class="container">
-            <a class="navbar-brand" href="#">Traversy Media</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/php-crash/feedback/index.html">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/php-crash/feedback/feedback.html">Feedback</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/php-crash/feedback/about.html">About</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php 
+        include "inc/header.php";
+    ?>
 
     <main>
         <div class="container d-flex flex-column align-items-center">
@@ -65,13 +46,9 @@
         </div>
     </main>
 
-    <footer class="text-center mt-5">
-        Copyright &copy; 2022
-    </footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
+    <?php
+        include "inc/footer.php"
+    ?>
 
 </body>
 
---
+++ inc/header.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-sm navbar-light bg-light mb-4">
+        <div class="container">
+            <a class="navbar-brand" href="#">Traversy Media</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../../php-feedback/index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../../php-feedback/feedback.php">Feedback</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../../php-feedback/about.php">About</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
