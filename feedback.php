c602c1829f00613ae9cc926ed9884b76f86d69b5
JairoCalderon <j.calderoneng@gmail.com>
2023-02-09 09:57:38 -0500

    Displaying hardcoding comments to the Feedback Page

--- feedback.php
+++ feedback.php
@@ -15,34 +15,49 @@
         include "inc/header.php";
     ?>
 
+
     <main>
         <div class="container d-flex flex-column align-items-center">
 
-            <h2>Feedback</h2>
+            <?php
+                $feedback = [
+                    [
+                        "id"=> "1",
+                        "name"=> "Beth Williams",
+                        "email"=> "beth@gmail.com",
+                        "body"=> "TV shows sucks"
+                    ],
+                    [
+                        "id"=> "2",
+                        "name"=> "Walt William",
+                        "email"=> "walt@gmail.com",
+                        "body"=> "TV shows are great"
+                    ],
+                    [
+                        "id"=> "3",
+                        "name"=> "Bill Johnson",
+                        "email"=> "bill@gmail.com",
+                        "body"=> "TV shows are a wate of time"
+                    ]
+                ];
+            ?>
 
-            <div class="card my-3">
-                <div class="card-body">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem
-                    repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore,
-                    totam labore maxime perferendis!
-                </div>
-            </div>
+            <h2>Past Feedback</h2>
 
-            <div class="card my-3">
-                <div class="card-body">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem
-                    repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore,
-                    totam labore maxime perferendis!
-                </div>
-            </div>
+            <?php if(empty($feedback)): ?>
+                <p class="lead mt3">There is no feedback</p>
+            <?php endif ?>
 
-            <div class="card my-3">
-                <div class="card-body">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem
-                    repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore,
-                    totam labore maxime perferendis!
-                </div>
-            </div>
+            <?php foreach($feedback as $item): ?>
+                <div class="card my-3 w-75">
+                    <div class="card-body text-center">
+                        <?php echo $item["body"] ?>
+                        <div class="text-secondary mt-2">
+                            By <?php echo $item["name"] ?>
+                        </div>
+                    </div>
+                </div>  
+            <?php endforeach ?>          
         </div>
     </main>
 
